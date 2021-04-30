9d4b3b137a67636731cfbd486088715432f47541
Anna Dyrduł <ania.dyrdul1905@gmail.com>
2021-04-30 11:47:51 +0200

    Example - io, file reader

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
@@ -0,0 +1,31 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReaderWithoutHandling {
+
+    public void readFile() throws FileReaderException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("names.txt").getFile());
+        Path path = Paths.get("tego-pliku-nie-ma.txt"); //blad
+
+        //try(Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+        try { //blad
+            Stream<String> fileLines = Files.lines(path);
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            throw new FileReaderException();
+        } finally {
+            System.out.println("I am gonna be here... always!");
+        }
+
+
+
+
+    }
+}
