51ccd81342c8888cf71158227b4b941b80d033a9
Anna Dyrduł <ania.dyrdul1905@gmail.com>
2021-06-06 22:49:11 +0200

    14.2.

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,2 +1,38 @@
-package com.kodilla.spring.calculator;public class Calculator {
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+    @Autowired
+    private Display display;
+
+    public double add(double a, double b) {
+        double resultOfAdd = a + b;
+        display.displayValue(resultOfAdd);
+        return resultOfAdd;
+    }
+
+    public double sub(double a, double b) {
+        double resultOfSub = a - b;
+        display.displayValue(resultOfSub);
+        return resultOfSub;
+    }
+
+    public double mul(double a, double b) {
+        double resultOfMul = a * b;
+        display.displayValue(resultOfMul);
+        return resultOfMul;
+    }
+
+    public double div(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        double resultOfDiv = a / b;
+        display.displayValue(resultOfDiv);
+        return resultOfDiv;
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,2 +1,58 @@
-package com.kodilla.spring.calculator;public class CalculatorTestSuite {
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void testAdd() {
+        //Given
+        //When
+        double result = calculator.add(1,2);
+        //Then
+        assertEquals(3, result);
+    }
+
+    @Test
+    void testSub() {
+        //Given
+        //When
+        double result = calculator.sub(-3,2);
+        //Then
+        assertEquals(-5, result);
+    }
+
+    @Test
+    void testMul() {
+        //Given
+        //When
+        double result = calculator.mul(1,2);
+        //Then
+        assertEquals(2, result);
+    }
+
+    @Test
+    void testDiv() {
+        //Given
+        //When
+        double result = calculator.div(1,2);
+        //Then
+        assertEquals(0.5, result);
+    }
+
+    @Test
+    void testDivButWithZero() {
+        //Given
+        //When
+        //Then
+        assertThrows(ArithmeticException.class, () -> calculator.div(1,0));
+    }
 }
