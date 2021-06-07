3d00bb31f2ccbe9277139194387caf75b5dbb2f7
Anna Dyrduł <ania.dyrdul1905@gmail.com>
2021-06-07 12:31:32 +0200

    14.3

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -5,23 +5,28 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+
 public final class Library {
 
     private final List<String> books = new ArrayList<>();
-    /*private LibraryDbController libraryDbController;
+    private LibraryDbController libraryDbController;
 
-    @Autowired
-    public void setLibraryDbController(LibraryDbController libraryDbController) {
+    public Library(final LibraryDbController libraryDbController) {
         this.libraryDbController = libraryDbController;
     }
 
     public Library() {
         //do nothing
-    }*/
+    }
+
+    /*@Autowired
+    private LibraryDbController libraryDbController;
 
     @Autowired
-    private LibraryDbController libraryDbController;
+    public void setLibraryDbController(LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+    */
 
     public void saveToDb() {
         libraryDbController.saveData();
