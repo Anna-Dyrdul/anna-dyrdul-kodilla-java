0fdea4b7378b5ddf0c1606dc7476840e1255938c
Anna Dyrduł <ania.dyrdul1905@gmail.com>
2021-07-14 19:04:06 +0200

    17.4. -do poprawy

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -0,0 +1,59 @@
+package com.kodilla.hibernate.manytomany;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@NamedNativeQuery(
+        name = "Company.retrieveWithFirstThreeLetter",
+        query = "SELECT * FROM COMPANIES" +
+                "WHERE SUBSTRING(COMPANY_NAME, 1, 3)=:FIRST_THREE_LETTERS",
+        resultClass = Company.class
+)
+@Entity
+@Table(name = "COMPANIES")
+public class Company {
+
+    private int id;
+    private String name;
+    private List<Employee> employees = new ArrayList<>();
+
+    public Company() {
+    }
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "COMPANY_ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "companies")
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    @NotNull
+    @Column(name = "COMPANY_NAME")
+    public String getName() {
+        return name;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+}
